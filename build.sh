b2946b1186b278df6d779ffb95b8ca2685e407da
Manuel Braun <thembrown@gmail.com>
2013-02-25 00:26:26 +0100

    minor compiler setting optimizations

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ emcc -c -I$ICONV_INCLUDE src/iconv_string.c && \
   emcc -c -I$ICONV_INCLUDE src/convert.cpp && \
   emcc -O2 -s WARN_ON_UNDEFINED_SYMBOLS=1 \
   -s EXPORTED_FUNCTIONS="['_convert_c', '_malloc', '_free']" \
+  -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="['malloc', 'free']" \
+  -s CLOSURE_ANNOTATIONS=1 \
+  -s TOTAL_MEMORY=2097152 \
+  -s TOTAL_STACK=524288 \
   convert.o iconv_string.o $ICONV_LIB --post-js src/wrapper.js -o iconv.js
 
 
