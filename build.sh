5952c54e48d35828f078935749f626281610cad1
Manuel Braun <thembrown@gmail.com>
2013-02-24 21:17:49 +0100

    added code

--- build.sh
+++ build.sh
@@ -5,8 +5,17 @@ ICONV_INCLUDE=../libiconv_js/build/include
 ICONV_LIB=../libiconv_js/build/lib/.libs/libiconv.a
 
 # /opt/emscripten/tools/bindings_generator.py bindings convert.h
-emcc -c -I$ICONV_INCLUDE iconv_string.c && \
-  emcc -c -I$ICONV_INCLUDE convert.cpp && \
+emcc -c -I$ICONV_INCLUDE src/iconv_string.c && \
+  emcc -c -I$ICONV_INCLUDE src/convert.cpp && \
   emcc -O2 -s WARN_ON_UNDEFINED_SYMBOLS=1 \
   -s EXPORTED_FUNCTIONS="['_convert_c', '_malloc', '_free']" \
-  convert.o iconv_string.o $ICONV_LIB --post-js wrapper.js -o iconv.js
+  convert.o iconv_string.o $ICONV_LIB --post-js src/wrapper.js -o iconv.js
+
+# emcc -c -I$ICONV_INCLUDE test.cpp
+# emcc -s WARN_ON_UNDEFINED_SYMBOLS=1 \
+# test.o convert.o iconv_string.o $ICONV_LIB -o test.html
+
+# cleanup
+rm -f convert.o iconv_string.o
+
+testacular start --auto-watch=false --single-run=true
